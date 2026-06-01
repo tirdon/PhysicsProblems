1f77bc86181d5a9f31bc7abc500437139d94d99f
tirdon <tirdonmugmo@gmail.com>
2026-06-01 19:48:14 +0700

    web worker works!

--- .src/Sources/PhysicsProblems/PhysicsProblems.swift
+++ .src/Sources/PhysicsProblems/PhysicsProblems.swift
@@ -1,13 +1,23 @@
-import JavaScriptKit
 import JavaScriptEventLoop
+import JavaScriptKit
+
+// swift package --package-path .src --scratch-path .build --swift-sdk 6.3-RELEASE-wasm32-unknown-wasip1-threads --allow-writing-to-directory script  js --use-cdn  --output script
+
+typealias DedicatedTaskExecutor = JavaScriptEventLoop
 
 @main
 struct PhysicsProblems {
-	static func main() {
-		JavaScriptEventLoop.installGlobalExecutor()
+	static func main() throws {
+		DedicatedTaskExecutor.installGlobalExecutor()
 		
-        print("Hello, world!")
+		print("Hello, world!")
 		Task {
+#if wasi_pthread
+//			let executor = try await WebWorkerDedicatedExecutor()
+			let executor = try await WebWorkerTaskExecutor(numberOfThreads: 1)
+			
+			defer { executor.terminate() }
+#endif
 			print("task")
 			await asdf()
 			print("tasks")
@@ -20,3 +30,21 @@ struct PhysicsProblems {
 nonisolated func asdf() async {
 	print("asdfasdf")
 }
+
+
+#if canImport(wasi_pthread)
+import wasi_pthread
+import WASILibc
+
+/// Trick to avoid blocking the main thread. pthread_mutex_lock function is used by
+/// the Swift concurrency runtime.
+@_cdecl("pthread_mutex_lock")
+func pthread_mutex_lock(_ mutex: UnsafeMutablePointer<pthread_mutex_t>) -> Int32 {
+	// DO NOT BLOCK MAIN THREAD
+	var ret: Int32
+	repeat {
+		ret = pthread_mutex_trylock(mutex)
+	} while ret == EBUSY
+	return ret
+}
+#endif
